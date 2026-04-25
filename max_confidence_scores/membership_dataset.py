75b20d52ee1ad57a795d071945b31e1d7eabd267
fatimahanif <fatimahanif303@gmail.com>
2026-04-25 15:23:37 +0200

    max confidence score testing for public dataset

---
+++ max_confidence_scores/membership_dataset.py
@@ -0,0 +1,30 @@
+from torch.utils.data import Dataset
+
+# dataset classes
+class TaskDataset(Dataset):
+    def __init__(self, transform=None):
+        self.ids = []
+        self.imgs = []
+        self.labels = []
+        self.transform = transform
+
+    def __getitem__(self, index):
+        id_ = self.ids[index]
+        img = self.imgs[index]
+        if self.transform is not None:
+            img = self.transform(img)
+        label = self.labels[index]
+        return id_, img, label
+
+    def __len__(self):
+        return len(self.ids)
+
+
+class MembershipDataset(TaskDataset):
+    def __init__(self, transform=None):
+        super().__init__(transform)
+        self.membership = []
+
+    def __getitem__(self, index):
+        id_, img, label = super().__getitem__(index)
+        return id_, img, label, self.membership[index]
